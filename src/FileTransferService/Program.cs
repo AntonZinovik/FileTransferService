891c1bdda1ea615ce8d36f2f09efcdcebd7ae8bf
ZinovikAV <zinovik33@gmail.com>
2023-11-06 15:31:48 +0300

    Инициализация проекта

---
+++ src/FileTransferService/Program.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+
+using Microsoft.OpenApi.Models;
+
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Service for sending large files",
+        Description = "ASP.NET Core Web API для проверки работы сервиса."
+    });
+
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+});
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom
+    .Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
