ce92a2a2a166666661d5d281cb96dd97fddd387d
ZinovikAV <zinovik33@gmail.com>
2023-11-14 17:42:03 +0300

    Добавлено расширение для Stream
    Добавлено расширение вычисляющие хэш-сумму;

---
+++ src/FileTransferService/Extensions/SteamExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace FileTransferService.Extensions;
+
+using System.Security.Cryptography;
+
+/// <summary>
+/// Расширение для потока данных.
+/// </summary>
+public static class SteamExtension
+{
+    /// <summary>
+    /// Вычисление хэш-суммы файла.
+    /// </summary>
+    /// <param name="stream">Поток даннах файла.</param>
+    /// <returns>Хэш-сумма.</returns>
+    public static async Task<byte[]> CalculationHashCodeAsync(this Stream stream)
+    {
+        return await SHA512.HashDataAsync(stream);
+    }
+}
