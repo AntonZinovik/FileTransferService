304953a3315cafa12affb19d86139fb330844289
ZinovikAV <zinovik33@gmail.com>
2023-11-16 15:27:50 +0300

    Исправление замечаний

--- src/FileTransferService/Extensions/StreamExtensions.cs
+++ src/FileTransferService/Extensions/StreamExtensions.cs
@@ -5,15 +5,16 @@ using System.Security.Cryptography;
 /// <summary>
 /// Расширение для потока данных.
 /// </summary>
-public static class SteamExtension
+public static class StreamExtensions
 {
     /// <summary>
     /// Вычисление хэш-суммы файла.
     /// </summary>
-    /// <param name="stream">Поток даннах файла.</param>
+    /// <param name="stream">Поток данных файла.</param>
     /// <returns>Хэш-сумма.</returns>
     public static async Task<byte[]> CalculationHashCodeAsync(this Stream stream)
     {
-        return await SHA512.HashDataAsync(stream);
+        var hashData = await SHA512.HashDataAsync(stream);
+        return hashData;
     }
 }
